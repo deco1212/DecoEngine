aae9c39d694dd2cc0cec9b79208fedd495fc45dc
DecoTheEpicGuy <andreethangamer@gmail.com>
2023-04-10 09:21:17 -0300

    added me in the credits!!!
    i am such a smart nerd modifying this source code on a chromebook...

--- source/CreditsState.hx
+++ source/CreditsState.hx
@@ -81,6 +81,8 @@ class CreditsState extends MusicBeatState
 		#end
 
 		var pisspoop:Array<Array<String>> = [ //Name - Icon name - Description - Link - BG Color
+			['Deco Engine Team'],
+			['DecoTheEpicGuy',		'shadowmario',		'Main Programmer of Psych Engine',								'https://www.youtube.com/channel/UCho7LiIpeZmzdlNXaKdZurg',	'444444'],
 			['Psych Engine Team'],
 			['Shadow Mario',		'shadowmario',		'Main Programmer of Psych Engine',								'https://twitter.com/Shadow_Mario_',	'444444'],
 			['RiverOaken',			'river',			'Main Artist/Animator of Psych Engine',							'https://twitter.com/RiverOaken',		'B42F71'],
